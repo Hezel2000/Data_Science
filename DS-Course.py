da70b405368abc96ad3e382d030077a0833672ec
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-06-26 18:38:58 +0200

    x

--- DS-Course.py
+++ DS-Course.py
@@ -6,6 +6,7 @@ import streamlit as st
 from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
+import numpy as np
 
 #---------------------------------#
 #------ Start --------------------#
@@ -14,21 +15,40 @@ import pandas as pd
 def start():
     st.subheader('Willkommen bei Data Science ')
     st.write('*für Mineralogen, Kosmo-/Geochemiker, Petrologen & den ganzen Rest*')
+    
+    st.write('''
+             **Wie zu schauen ist – oder zumindest: wie geschaut werden kann**\n
+Anhalten, nachvollziehen, eventuell zurückspulen und dazu eigen Skizzen oder Mitschriebe anfertigen
+
+**Die Veranstaltung besteht aus**\n
+ca. 100 Seiten Skript, aufgeteilt in die 3 Teilskripte
+ca. 50 Videos  mit >7 Stunden Material
+über 100 Jupyter Notebooks  mit >100 Fragen, Aufgaben & Beispielen
+>100 Selbstlern-Fragen mit Antworten
+>20 Aufgaben zur gemeinsamen Lösung in den Präsenzphasen und den Semester-Projekten
+zahlreiche zusätzlicher Dateien für verschiedene Aufgaben
+
+**Manche Videos haben aus rechtlichen Gründen ein Passwort, das lautet:**\n
+ Passwort für einige wenige Videos: das erhaltet Ihr von mir
+
+Einordnung der Einheiten für die Abschlussprüfung:
+Ich ergänze die Überschriften um die beiden Symbole ◉ (Relevanz) und ▲ (Verständnis). Die Symbole haben eine von 3 Farben, welche anzeigen, wie relevant oder wichtig für das Verständnis ein Thema ist. Die Farben sind: orange: hoch, grün: mittel, blau: weniger. 
+
+             ''')
 
 
 #---------------------------------#
 #------ Globale Suche ------------#
 #---------------------------------#
-def globale_suche():
+def Vorlesungen_Uebungen():
     import streamlit as st
     st.subheader('Wähle Deine Einheit')
 
-    st.legacy_caching.clear_cache()
     @st.cache
     def importCourseDatasheet():
         dfSearchAll= pd.read_csv('https://raw.githubusercontent.com/Hezel2000/Data_Science/main/course_material.csv')
         return dfSearchAll
-
+    
     
     def useCourse(dfSearchAll):
         dfSearchAll = dfSearchAll
@@ -42,33 +62,29 @@ def globale_suche():
     
         if len(sel_row) > 0:    
             
-            st.write(sel_row[0]['Exercise ipynb'])
-            
             col1, col2 = st.columns([3, 1])
             with col1:
                 st.video(sel_row[0]['Youtube'])
             with col2:
+                st.write('Laufzeit: ' + sel_row[0]['Laufzeit'])
                 with st.expander('Jupyter Notebooks', expanded=True):
-                    st.write("[Vorlesung](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Vorlesung ipynb'] + ")")
-                    st.write("[Lösungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Übungen ipynb'] + ")")
-                    st.write("[Lösungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Lösungen ipynb'] + ")")
-                with st.expander('Resources', expanded=True):
-                    ('noch extra was')
-                with st.expander('Downloads', expanded=True):
-                    ('noch was')            
-                with st.expander('Keywords', expanded=True):
-                    ('noch extra was')
+                    if np.isnan(sel_row[0]['Vorlesung ipynb']):
+                        vorlesung = "[Vorlesung](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Vorlesung ipynb'] + ")"
+                    else:
+                        vorlesung=sel_row[0]['Vorlesung ipynb']
+                    st.write(vorlesung,
+                             "[Übungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Übungen ipynb'] + ")",
+                             "[Lösungen](https://raw.githubusercontent.com/Hezel2000/Data_Science/main/jupyter_nb/" + sel_row[0]['Lösungen ipynb'] + ")")
+                with st.expander('Schlagworte', expanded=True):
+                    st.write(sel_row[0]['Schlagworte'])
             
             #st.subheader('Beschreibung')
-            
-            st.write(sel_row[0]['Übung ipynb'])
-            
             st.write(sel_row[0]['Beschreibung'])
     
         else:
             st.subheader('Wähle eine Einheit aus obiger Liste')
             
-    
+
     dfSearchAll = importCourseDatasheet()
 
     useCourse(dfSearchAll)
@@ -124,8 +140,8 @@ def kapitel_3():
 
 
 page_names_to_funcs = {
-    'Start': start,
-    'Globale Suche': globale_suche,
+    'Einführung': start,
+    'Vorlesungen & Übungen': Vorlesungen_Uebungen,
     'Kapitel 1': kapitel_1,
     'Kapitel 2': kapitel_2,
     'Kapitel 3': kapitel_3,
