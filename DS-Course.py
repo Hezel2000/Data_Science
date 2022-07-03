2c9c5dda346ae289348f12068e855a6accdc3f87
Hezel2000 <86461739+Hezel2000@users.noreply.github.com>
2022-07-03 15:48:56 +0200

    x

--- DS-Course.py
+++ DS-Course.py
@@ -6,18 +6,20 @@ from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# =============================================================================
+# hide_st_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             header {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
+# =============================================================================
 
 
 #---------------------------------#
-#------ Einführung --------------#
+#------ Einführung ---------------#
 #---------------------------------#
     
 def einfuehrung():
@@ -31,7 +33,7 @@ def einfuehrung():
 #---------------------------------#
 def Vorlesungen_Uebungen():
     import streamlit as st
-    st.subheader('Wähle Deine Einheit')
+    st.subheader('Wähle Deine Lerneinheit')
 
     @st.cache
     def importCourseDatasheet():
@@ -135,66 +137,6 @@ def inhaltsverzeichnis():
     st.write('*coming soon ...*')
     
     
-    
-#---------------------------------#
-#------ Test  --------------------#
-#---------------------------------#  
-def test():
-    import numpy as np
-    from bokeh.layouts import column, row
-    from bokeh.models import CustomJS, Slider, Select
-    from bokeh.plotting import ColumnDataSource, figure
-    
-    st.header('Testing')
-    
-    x = np.linspace(0, 10, 500)
-    y = np.sin(x)
-    
-    source = ColumnDataSource(data=dict(x=x, y=y))
-    
-    plot = figure(y_range=(-10, 10), width=400, height=400)
-    
-    plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-    plot.xaxis.axis_label='x-axis'
-    plot.xaxis.axis_label_text_font_size = '20px'
-    
-    amp_slider = Slider(start=0.1, end=10, value=1, step=.1, title="Amplitude")
-    freq_slider = Slider(start=0.1, end=10, value=1, step=.1, title="Frequency")
-    phase_slider = Slider(start=0, end=6.4, value=0, step=.1, title="Phase")
-    offset_slider = Slider(start=-5, end=5, value=0, step=.1, title="Offset")
-    
-    callback = CustomJS(args=dict(source=source, amp=amp_slider, freq=freq_slider, phase=phase_slider, offset=offset_slider),
-                        code="""
-        const data = source.data;
-        const A = amp.value;
-        const k = freq.value;
-        const phi = phase.value;
-        const B = offset.value;
-        const x = data['x']
-        const y = data['y']
-        for (let i = 0; i < x.length; i++) {
-            y[i] = B + A*Math.sin(k*x[i]+phi);
-        }
-        source.change.emit();
-    """)
-    
-    amp_slider.js_on_change('value', callback)
-    freq_slider.js_on_change('value', callback)
-    phase_slider.js_on_change('value', callback)
-    offset_slider.js_on_change('value', callback)
-    
-    select = Select(title="x-axis", value="Yellow", options=["Red", "Yellow", "Blue", "Green"])
-    #select.js_link('value', plot.xaxis, 'axis_label')
-    
-    layout = row(
-        column(amp_slider, freq_slider, phase_slider, offset_slider, select),
-        plot
-    )
-    
-    st.bokeh_chart(layout)
-    st.subheader('Inhaltsverzeichnis')
-
-
     
 #---------------------------------#
 #------ Main Page Sidebar --------#
